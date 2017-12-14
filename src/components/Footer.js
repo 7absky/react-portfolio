ab4190fa82fbc0bb78f041b84e2c98515ca2f20a
Patryk Laba <patryk.laba@icloud.com>
2017-12-14 22:07:18 +0100

    Added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+const Footer = (props) => {
+    return (
+        <div className="footer">
+            <span id="credentials">&copy; Patryk Łaba 2017</span> 
+        </div>
+    );
+};
+
+
+export default Footer;
