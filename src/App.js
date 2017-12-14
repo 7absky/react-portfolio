496d6e36d287dac2638f2570471900501a5395fd
Patryk Laba <patryk.laba@icloud.com>
2017-12-14 22:07:57 +0100

    Styling and importing components to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from "./components/Nav";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
+        <Content />
+        <Footer />
       </div>
     );
   }
