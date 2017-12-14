e761bb39458e6a4448ad744097b10b040ce4573b
Patryk Laba <patryk.laba@icloud.com>
2017-12-14 21:43:47 +0100

    Added Navigation component

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+const Nav = (props) => {
+    return (
+        <div className="navigation">
+            
+        </div>
+    );
+};
+
+
+export default Nav;
