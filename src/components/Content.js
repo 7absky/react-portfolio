f5af11d9ef5f2e385a87f3e1339b3c13f6e6205a
Patryk Laba <patryk.laba@icloud.com>
2017-12-14 22:07:35 +0100

    Added Content component

---
+++ src/components/Content.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Content extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return(
+            <div className="content-wrapper">
+                i am a wrapper!
+            </div>
+        );
+    }
+}
+
+export default Content;
